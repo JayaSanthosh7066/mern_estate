f0d6fa0608bff645e7e02bfad60776d5b7173386
JayaSanthosh7066 <jayasanthoshamalakanti@gmail.com>
2024-01-21 15:53:50 +0530

    second

--- api/index.js
+++ api/index.js
@@ -1,4 +1,10 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log("Connected to DataBase");
+});
 const app = express();
 app.listen(3000, () => {
   console.log(`Server is listening at port ${3000}`);
