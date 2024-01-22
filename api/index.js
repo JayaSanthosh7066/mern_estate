9746cec03a2fca88c85e46197d06ee858d2960ca
JayaSanthosh7066 <jayasanthoshamalakanti@gmail.com>
2024-01-22 10:19:35 +0530

    third

--- api/index.js
+++ api/index.js
@@ -2,12 +2,15 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import userAuth from "./routes/auth.route.js";
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(() => {
   console.log("Connected to DataBase");
 });
 const app = express();
+app.use(express.json());
 app.listen(3000, () => {
   console.log(`Server is listening at port ${3000}`);
 });
 app.use("/api/user", userRouter);
+app.use("/api/auth", userAuth);
