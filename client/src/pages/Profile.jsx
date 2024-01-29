91b0b8271abb20fb2e22ec9cc7d1cbecf56eb8f6
JayaSanthosh7066 <jayasanthoshamalakanti@gmail.com>
2024-01-29 22:47:38 +0530

    delete_listings

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -133,6 +133,23 @@ const Profile = () => {
       setShowListingsError(true);
     }
   };
+  const handleListingDelete = async (listingid) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingid}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingid)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center"> Profile</h1>
@@ -241,8 +258,13 @@ const Profile = () => {
                 <p>{listing.name}</p>
               </Link>
               <div className="flex flex-col items-center">
-                <div className="text-red-700 uppercase">Delete</div>
-                <div className="text-green-700 uppercase">Edit</div>
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className="text-red-700 uppercase"
+                >
+                  Delete
+                </button>
+                <button className="text-green-700 uppercase">Edit</button>
               </div>
             </div>
           ))}
