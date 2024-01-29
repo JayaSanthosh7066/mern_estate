0f7ad0a2b9ced1a5546a04b372493157db8baf54
JayaSanthosh7066 <jayasanthoshamalakanti@gmail.com>
2024-01-29 11:40:08 +0530

    listings_api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/User.model.js";
+import Listing from "../models/Listing.model.js";
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from "bcryptjs";
 export const test = (req, res) => {
@@ -44,5 +45,14 @@ export const deleteUser = async (req, res, next) => {
     res.status(200).json("User has been deleted!");
   } catch (error) {
     next(error);
+  }
+};
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    const listings = await Listing.find({ userRef: req.params.id });
+    res.status(200).json(listings);
+  } else {
+    return next(errorHandler(401, "You can only view your listings"));
   }
 };
