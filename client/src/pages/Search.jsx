8edcb1f02fb50c749ca1690e2af97177bc1e10fb
JayaSanthosh7066 <jayasanthoshamalakanti@gmail.com>
2024-02-02 07:49:52 +0530

    showmore_listings

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -108,6 +108,19 @@ export default function Search() {
     const searchQuery = urlParams.toString();
     navigate(`/search?${searchQuery}`);
   };
+  const onShowMoreClick = async () => {
+    const numberOfListings = listings.length;
+    const startIndex = numberOfListings;
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set("startIndex", startIndex);
+    const searchQuery = urlParams.toString();
+    const res = await fetch(`/api/listing/get?${searchQuery}`);
+    const data = await res.json();
+    if (data.length < 9) {
+      setShowMore(false);
+    }
+    setListings([...listings, ...data]);
+  };
   return (
     <div className="flex flex-col md:flex-row">
       <div className="p-7  border-b-2 md:border-r-2 md:min-h-screen">
@@ -231,6 +244,14 @@ export default function Search() {
               <Listingitem key={listing._id} listing={listing} />
             ))}
         </div>
+        {showMore && (
+          <button
+            onClick={onShowMoreClick}
+            className="text-green-700 hover:underline p-7 text-center w-full"
+          >
+            Show more
+          </button>
+        )}
       </div>
     </div>
   );
